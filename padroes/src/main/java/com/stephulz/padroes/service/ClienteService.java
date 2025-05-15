226278b15e4372759b6fe3834847f979abb4bc3e
Stephulz <stefansanches9@gmail.com>
2025-05-14 22:59:04 -0300

    feat: gof api user

---
+++ padroes/src/main/java/com/stephulz/padroes/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.stephulz.padroes.service;
+
+import com.stephulz.padroes.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
